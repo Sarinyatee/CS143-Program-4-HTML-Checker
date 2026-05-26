5b8c8c66d719899cf31c7d1a487ddd79d3f01bc4
Sarinyatee <sarinyatee@gmail.com>
2026-05-26 15:11:03 -0700

    delete unnecessary Queue and fixed indentation
    - Delete  the field html.
    - Delete Queue fixed in fixHTML()
    - fixed small indentation to easier reading

--- HTMLManager.java
+++ HTMLManager.java
@@ -1,62 +1,61 @@
 import java.util.*;
 
 public class HTMLManager {
-  private Queue<HTMLTag> tags;
-  public Queue<HTMLTag> html;
-  
-  public HTMLManager() {
-    tags = new LinkedList<>(); 
-  }
-  
-  public HTMLManager(Queue<HTMLTag> html) {
-    if(html == null) {
-      throw new IllegalArgumentException("It is empty HTML file.");
-    } 
-    tags = new LinkedList<>(html);
-  }
+   private Queue<HTMLTag> tags;
    
-  public Queue<HTMLTag> getTags() {
-    return tags; 
-  }
-  
-  public void fixHTML() {
-   Stack<HTMLTag> stack = new Stack<>();
-   Queue<HTMLTag> fixed = new LinkedList<>();
+   public HTMLManager() {
+      tags = new LinkedList<>(); 
+   }
    
-   while (!tags.isEmpty()) {
-      HTMLTag tag = tags.remove();
-      if(tag.isSelfClosing()) {
-         fixed.add(tag);
-      } else if(tag.isOpening()) {
-         fixed.add(tag);
-         stack.push(tag);
-      } else {
-         if(stack.isEmpty()) {
-         // No matching open tag — do nothing
-         } else if (stack.peek().matches(tag)) {
-            fixed.add(tag);
-            stack.pop();
+   public HTMLManager(Queue<HTMLTag> html) {
+      if (html == null) {
+         throw new IllegalArgumentException("It is empty HTML file.");
+      }
+      tags = new LinkedList<>(html);
+   }
+   
+   public Queue<HTMLTag> getTags() {
+      return tags; 
+   }
+   
+   public void fixHTML() {
+      Stack<HTMLTag> stack = new Stack<>();
+      int size = tags.size();
+      
+      for (int i = 0; i < size; i++) {
+         HTMLTag tag = tags.remove();
+         
+         if (tag.isSelfClosing()) {
+            tags.add(tag);
+         } else if (tag.isOpening()) {
+            tags.add(tag);
+            stack.push(tag);
          } else {
-            fixed.add(stack.pop().getMatching());
-            tags.add(tag);                                        
+            if (stack.isEmpty()) {
+            // No matching open tag — do nothing        
+            } else if (stack.peek().matches(tag)) {
+               tags.add(tag);
+               stack.pop();
+            } else {
+            tags.add(stack.pop().getMatching());   
+            }
          }
       }
+
+      while (!stack.isEmpty()) {
+         tags.add(stack.pop().getMatching());
+      }
+    }
+
+   public String toString() {
+      String result = "";
+      int size = tags.size();
+      
+      for (int i = 0; i < size; i++) {
+         HTMLTag tag = tags.remove();
+         result += tag.toString().trim() + "\n";
+         tags.add(tag);      
+      }    
+      return result;
    }
- 
-   while (!stack.isEmpty()) {
-      fixed.add(stack.pop().getMatching());
-   }
-   tags = fixed;
-  }
-   
-  public String toString() {
-   String result = "";
-        int size = tags.size();
-        for (int i = 0; i < size; i++) {
-            HTMLTag tag = tags.remove();
-            result += tag.toString().trim() + "\n";
-            tags.add(tag);      
-    }    
-    return result;
-  }
 }
